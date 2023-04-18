1a823d26a83283c1662412e6eeb706e809fc7130
Agustina <agustina.leon@productminds.io>
2023-04-18 14:36:17 -0300

    Agrego Firebase integration for push notifications and Braze push integration

---
+++ app/src/main/java/com/example/myapplication/HomePageActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.braze.Braze
+
+class HomePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+        Braze.getInstance(this).openSession(this)
+
+        // get reference to button
+        val btnPushPermission = findViewById<Button>(R.id.button_ask_push_permission)
+        // set on-click listener
+        btnPushPermission.setOnClickListener {
+            Toast.makeText(this@HomePageActivity, "Asking for push permission", Toast.LENGTH_SHORT).show()
+        }
+
+        // get reference to button
+        val btnLocationPermission = findViewById<Button>(R.id.button_ask_location_permission)
+        // set on-click listener
+        btnLocationPermission.setOnClickListener {
+            Toast.makeText(this@HomePageActivity, "Asking for location permission", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Braze.getInstance(this).closeSession(this)
+    }
+
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
-
-import com.braze.Braze;
-
+import androidx.appcompat.app.AppCompatActivity
+import com.braze.Braze
 import com.example.myapplication.R.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,28 +19,11 @@ class MainActivity : AppCompatActivity() {
         // set on-click listener
         btnStartSession.setOnClickListener {
             Braze.getInstance(this).openSession(this)
+            val intent = Intent(applicationContext, HomePageActivity::class.java)
+            startActivity(intent)
             Toast.makeText(this@MainActivity, "Starting Session", Toast.LENGTH_SHORT).show()
         }
 
-        val btnEndSession = findViewById<Button>(id.button_end_session)
-        // set on-click listener
-        btnEndSession.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Ending Session", Toast.LENGTH_SHORT).show()
-        }
-
-        // get reference to button
-        val btnPushPermission = findViewById<Button>(id.button_ask_push_permission)
-        // set on-click listener
-        btnPushPermission.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Asking for push permission", Toast.LENGTH_SHORT).show()
-        }
-
-        // get reference to button
-        val btnLocationPermission = findViewById<Button>(id.button_ask_location_permission)
-        // set on-click listener
-        btnLocationPermission.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Asking for location permission", Toast.LENGTH_SHORT).show()
-        }
     }
 
     override fun onStop() {
